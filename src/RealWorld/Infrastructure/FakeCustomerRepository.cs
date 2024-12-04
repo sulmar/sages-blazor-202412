1bad8b33a297205585c539fb01e1bd7cf51e6494
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-12-04 11:33:53 +0100

    Dodanie projektu infrastruktury

---
+++ src/RealWorld/Infrastructure/FakeCustomerRepository.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Abstractions;
+using Domain.Models;
+
+namespace Infrastructure;
+
+public class FakeCustomerRepository : ICustomerRepository
+{
+    private IDictionary<int, Customer> _customers;
+
+    public FakeCustomerRepository(IEnumerable<Customer> customers)
+    {
+       
+
+        _customers = customers.ToDictionary(p => p.Id);
+    }
+
+    public IEnumerable<Customer> GetAll()
+    {
+        return _customers.Values.ToList();
+    }
+}
+
+
+public class DbCustomerRepository : ICustomerRepository
+{
+    public IEnumerable<Customer> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+}
