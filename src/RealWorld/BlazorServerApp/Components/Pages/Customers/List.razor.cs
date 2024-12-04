b08cb14d0972da1abb2d52df492227ab942286ca
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-12-04 12:32:43 +0100

    Dodanie partial class

---
+++ src/RealWorld/BlazorServerApp/Components/Pages/Customers/List.razor.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Abstractions;
+using Domain.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorServerApp.Components.Pages.Customers;
+
+public partial class List
+{
+    // .NET 9
+    //public List(ICustomerRepository Repository)
+    //{
+    //    this.Repository = Repository;
+    //}
+
+    [Inject]
+    private ICustomerRepository Repository {  get; set; }
+
+    private IEnumerable<Customer> Customers { get; set; }
+
+    protected override void OnInitialized()
+    {
+        Customers = Repository.GetAll();
+    }
+
+}
