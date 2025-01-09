26d4c54a20e4d45b8b5aceb2126727ef5a60a6c4
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-01-09 15:16:17 +0100

    Dodanie formularza i walidacji za pomocą data annotations

--- src/RealWorld/Domain/Models/Customer.cs
+++ src/RealWorld/Domain/Models/Customer.cs
@@ -1,7 +1,14 @@
-﻿namespace Domain.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Models;
 
 public class Customer : BaseEntity
 {
+    [Required]
+    [MaxLength(10), MinLength(2)]
     public string FirstName { get; set; }
+
+    [Required, MaxLength(10), MinLength(3)]
     public string LastName { get; set; }
+    public string Note { get; set; }
 }
