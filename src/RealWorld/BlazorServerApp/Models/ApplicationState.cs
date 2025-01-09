67952f7a3fc5b7bf63da36b830b8a709ea986108
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-01-09 11:39:21 +0100

    Dodanie ApplicationState

---
+++ src/RealWorld/BlazorServerApp/Models/ApplicationState.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorServerApp.Models;
+
+public class ApplicationState
+{
+    public string LoggedUser { get; set; }
+    public DateTime LoggedOn { get; set; }
+    public string Theme { get; set; }    
+}
