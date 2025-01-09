032dd500fe900f15b4a26ef81307c80dad0741d0
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-01-09 11:21:56 +0100

    Implementacja kontekstu licznika za pomocą wzorca projektowego Singleton

--- src/RealWorld/BlazorServerApp/Models/CounterContext.cs
+++ src/RealWorld/BlazorServerApp/Models/CounterContext.cs
@@ -3,4 +3,21 @@
 public class CounterContext
 {
     public int CurrentCount { get; set; } = 5;
+
+
+    // Wzorzec projektowy Singleton
+    private static CounterContext _instance;
+    public static CounterContext Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = new CounterContext();
+
+            return _instance;
+
+        }
+    }
+
+   protected CounterContext() { }
 }
