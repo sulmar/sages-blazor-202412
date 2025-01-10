45066d91639a1e5a180a4f7835eee1c623fb86ef
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-01-10 10:38:06 +0100

    Dodanie dashboard

---
+++ src/RealWorld/Domain/Models/DashboardContext.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Models;
+
+public class DashboardContext
+{
+    public int TotalSales { get; set; }
+    public int NewUsers { get; set; }
+    public int ActiveSessions { get; set; }
+}
